2093238d7f22590e39b400e849a4a2dadf7bce7a
ARY <widhiarya@outlook.com>
2022-12-23 14:11:12 +0700

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireDatabaseModule } from "@angular/fire/compat/database";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import { PageStudentListComponent } from './pages/page-student-list/page-student-list.component';
+import {FormsModule} from "@angular/forms";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageStudentListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+        apiKey: "AIzaSyBjPP77PsHRn0GKJ0MiqIEEDSAYHyqMrAw",
+        authDomain: "oncom-berry.firebaseapp.com",
+        databaseURL: "https://oncom-berry-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "oncom-berry",
+        storageBucket: "oncom-berry.appspot.com",
+        messagingSenderId: "291322685143",
+        appId: "1:291322685143:web:ad7ce66eadecd7c0fd7292"
+      }
+    ),
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
